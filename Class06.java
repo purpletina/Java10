445d541efb2893c83e8f8720e58b62d421e85e6f
purpletina <113041666+purpletina@users.noreply.github.com>
2022-10-28 20:09:27 +0800

    Create Class06.java

---
+++ Class06.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package Java10;
+class CTriangle{
+	protected int base;
+	protected int height;
+	
+	public CTriangle(int b, int h) {
+		base = b;
+		height = h;
+	}
+	protected void show() {
+		System.out.println("base="+ base + ", " + "height=" + height);
+	}
+}
+
+class CData extends CTriangle{
+	public CData(int b, int h) {
+		super(b,h);
+}
+	public void area() {
+		show();
+		System.out.println("area=" + base*height/2.0);
+	}
+}
+public class J06 {
+	public static void main(String[] args) {
+		CData obj = new CData(3,8);
+		obj.area();
+	}
+
+}
